abce557e3559241bc6c5480c58a02de4dc6b024c
Sharon Korir <95223878+sharonkorir@users.noreply.github.com>
2022-07-06 10:27:24 +0000

    [LeetCode Sync] Runtime - 1858 ms (5.04%), Memory - 24.7 MB (99.95%)

---
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buying_price = prices[0]
+        max_profit = 0
+        
+        for i in range(len(prices)):
+            if buying_price > prices[i]:
+                buying_price = prices[i]
+            elif (prices[i] - buying_price) > max_profit:
+                max_profit = prices[i] - buying_price
+        return max_profit

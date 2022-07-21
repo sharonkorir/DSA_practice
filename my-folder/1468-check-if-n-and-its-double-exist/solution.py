bf6fd137ac824d5f92d06e869bf0c1eefddab6ee
Sharon Korir <95223878+sharonkorir@users.noreply.github.com>
2022-07-21 13:54:30 +0000

    [LeetCode Sync] Runtime - 35 ms (6.21%), Memory - 13.6 MB (4.18%)

---
+++ my-folder/1468-check-if-n-and-its-double-exist/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def checkIfExist(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        lookup = set()
+        for x in arr:
+            if 2*x in lookup or (x%2 == 0 and x//2 in lookup):
+                return True
+            lookup.add(x)
+        return False
+                

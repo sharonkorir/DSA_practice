760af604ef4e9aa4daa29010119af2a160e660f9
Sharon Korir <95223878+sharonkorir@users.noreply.github.com>
2022-10-17 10:39:09 +0000

    [LeetCode Sync] Runtime - 50 ms (5.29%), Memory - 13.5 MB (31.70%)

--- my-folder/1603-running-sum-of-1d-array/solution.py
+++ my-folder/1603-running-sum-of-1d-array/solution.py
@@ -1,13 +1,9 @@
 class Solution(object):
     def runningSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
         sum_nums = 0
         ans = []
         for i in range(len(nums)):
             sum_nums += nums[i]
             ans.append(sum_nums)
         return ans
-            
+

d2f661b9a2a1e014a870b2eac3ff24877531ee2a
Sharon Korir <95223878+sharonkorir@users.noreply.github.com>
2025-04-25 06:08:24 +0000

    [LeetCode Sync] Runtime - 19 ms (12.58%), Memory - 19 MB (48.80%)

--- my-folder/0026-remove-duplicates-from-sorted-array/solution.py
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,11 +1,13 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        k = 1
-        
-        for i in range(1,len(nums)):
-            if nums[i] != nums[i-1]:
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        set_list = []
+        k = 0 
+        for i in range(len(nums)):
+            if nums[i] not in set_list:
+                set_list.append(nums[i])
                 nums[k] = nums[i]
                 k += 1
         return k
-                
-        
+               
+
+
